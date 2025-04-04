bb8c450851e71584947b2268e8b26bd6075b5424
fency <fencyatf@gmail.com>
2025-04-04 13:07:59 +0530

    config cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ connectDB()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://movie-rating-and-review-client.onrender.com", //http://localhost:5173
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }));
